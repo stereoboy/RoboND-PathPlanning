ffc7acd951d860d7293e7316cc199c252a92dfa2
Jaeyoung Lee <jylee0121@gmail.com>
2018-06-17 01:31:46 +0900

    add wall_follower node
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ src/ShellScripts/wall_follower.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ABS_SCRIPT_PATH=$PWD/$(dirname "$0")
+echo "$ABS_SCRIPT_PATH"
+ABS_DEVEL_PATH=$ABS_SCRIPT_PATH/../../devel
+echo "$ABS_DEVEL_PATH"
+ABS_SRC_PATH=$ABS_SCRIPT_PATH/../../src
+echo "$ABS_SRC_PATH"
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash & roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$ABS_SRC_PATH/World/MyWorld.world " &
+sleep 5
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash & roslaunch wall_follower gmapping.launch" & 
+sleep 2
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash & roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 2
+#xterm  -e  " source $ABS_DEVEL_PATH/setup.bash & roslaunch turtlebot_teleop keyboard_teleop.launch"
+#sleep 2
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash & roslaunch wall_follower wall_follower.launch"
