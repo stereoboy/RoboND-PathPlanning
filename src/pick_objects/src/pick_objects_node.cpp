83b21204299cd4b9f4f0fc9519dff56e13933a3d
Jaeyoung Lee <jylee0121@gmail.com>
2018-06-24 13:24:54 +0900

    update pick_objects
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- src/pick_objects/src/pick_objects_node.cpp
+++ src/pick_objects/src/pick_objects_node.cpp
@@ -1,13 +1,14 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <tf/transform_datatypes.h>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "pick_objects");
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -19,26 +20,67 @@ int main(int argc, char** argv){
 
   move_base_msgs::MoveBaseGoal goal;
 
+  //-----------------------------------------------
+  // Go to the pick up zone
+  //-----------------------------------------------
   // set up the frame parameters
-  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.frame_id = "/map";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  goal.target_pose.pose.position.x = 3.75;
+  goal.target_pose.pose.position.y = 3.75;
+  goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(M_PI/2.0);
 
    // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
+  ROS_INFO("[ROBOT] Sending goal");
   ac.sendGoal(goal);
+  ROS_INFO("[ROBOT] Move...");
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("[ROBOT] Hooray, the robot arrived at the PICK-UP ZONE");
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("[ROBOT] The base failed to move to the PICK-UP ZONE for some reason");
 
+  // sleep for 5 seconds
+  ROS_INFO("[ROBOT] Picking objects...");
+  ros::Duration(5.0).sleep();
+
+  //-----------------------------------------------
+  // Go to the pick up zone
+  //-----------------------------------------------
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "/map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = 0.0;
+  goal.target_pose.pose.position.y = 0.0;
+  goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(-M_PI/2.0);
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("[ROBOT] Sending goal");
+  ac.sendGoal(goal);
+  ROS_INFO("[ROBOT] Move...");
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("[ROBOT] Hooray, the robot arrived at the DROP-OFF ZONE");
+  else
+    ROS_INFO("[ROBOT] The base failed to move to the DROP-OFF ZONE for some reason");
+  
+  // to prevent the terminal from closing up
+  ros::Duration time_between_ros_wakeups(0.2);
+  while (ros::ok()) {
+      ros::spinOnce();
+      time_between_ros_wakeups.sleep();
+  }
   return 0;
 }
