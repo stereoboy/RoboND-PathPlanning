8247727b2bf9e50f184a0ab0d98fa9be0e338b5e
Jaeyoung Lee <jylee0121@gmail.com>
2018-06-24 12:37:29 +0900

    add base line of pick_objects
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ src/ShellScripts/pick_objects.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ABS_SCRIPT_PATH=$PWD/$(dirname "$0")
+echo "$ABS_SCRIPT_PATH"
+ABS_DEVEL_PATH=$ABS_SCRIPT_PATH/../../devel
+echo "$ABS_DEVEL_PATH"
+ABS_SRC_PATH=$ABS_SCRIPT_PATH/../../src
+echo "$ABS_SRC_PATH"
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$ABS_SRC_PATH/World/MyWorld.world " &
+sleep 5
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$ABS_SRC_PATH/World/myMap8.yaml" & 
+sleep 2
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 2
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; rosrun pick_objects pick_objects_node"
+sleep 2
