ca7a69c3eb12f0c1bca0edc7d515ee95cad1775e
Jaeyoung Lee <jylee0121@gmail.com>
2018-07-02 00:34:41 +0900

    remove whitespace
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- src/ShellScripts/test_slam.sh
+++ src/ShellScripts/test_slam.sh
@@ -7,7 +7,7 @@ ABS_SRC_PATH=$ABS_SCRIPT_PATH/../../src
 echo "$ABS_SRC_PATH"
 xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$ABS_SRC_PATH/World/MyWorld.world " &
 sleep 5
-xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 2
 xterm  -e  " source $ABS_DEVEL_PATH/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 2
